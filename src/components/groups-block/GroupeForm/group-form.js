45393e17203f609e933e4768240ab21363378c84
unknown <armen.yesayan@beeoncode.com>
2021-03-22 16:33:22 +0400

    add price block

--- src/components/groups-block/GroupeForm/group-form.js
+++ src/components/groups-block/GroupeForm/group-form.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 import css from './group-form.module.css'
 import CloseIcon from '../../../icons/png/close-icon.png'
@@ -11,11 +11,8 @@ import flagUSA from "../../../icons/png/flag-USA.png";
 
 
 
-const GroupForm  = () => {
-    let [ShadeValue, setNewShadeValue] = useState([1,4]);
-    let [CleanlinessValue, setNewCleanlinessValue] = useState([2,6]);
-    let [internalNameValue, setInternalNameValue] = useState('');
-    let [forSaleNameValue, setForSaleNameValue] = useState('');
+const GroupForm  = ({ formCloser,cleanlinessHandleChanger, shadeHandleChanger, internalNameValue, forSaleNameValue, formHandler }) => {
+
     const marksShade  = [
         {
             value: 0,
@@ -89,79 +86,68 @@ const GroupForm  = () => {
         },
     ];
 
-    const shadeHandleChange = (event, newShadeValue) => {
-        setNewShadeValue(newShadeValue);
-    };
-    const CleanlinessHandleChange = (event, newCleanlinessValue) => {
-        setNewCleanlinessValue(newCleanlinessValue);
-    };
-    const internalNameValueHandler = (e) => {
-        setInternalNameValue(e.target.value)
-    };
-    const forSaleNameValueHandler = (e) => {
-        setForSaleNameValue(e.target.value)
-    };
+
 
     return (
-        <div className={css.groupForm}>
-            <div className={css.groupFormHeader}>
-                <p className={css.groupFormTitle}>
-                    группы
-                </p>
-                <span className={css.gropeFormClose}>
-                    <img src={CloseIcon} alt="close"/>
-                </span>
-            </div>
-            <form className={css.groupFormContent}>
-                <div className={css.groupformShade}>
-                    <p className={css.sliderTitle}>
-                        оттенок
-                    </p>
-                    <Slider
-                        onChange={shadeHandleChange}
-                        defaultValue={[1,4]}
-                        aria-labelledby="discrete-slider-custom"
-                        marks={marksShade}
-                        min={0}
-                        max={6}
-                    />
-                </div>
-                <div className={css.groupformCleanliness}>
-                    <p className={css.sliderTitle}>
-                        ​Чистота
-                    </p>
-                    <Slider
-                        onChange={CleanlinessHandleChange}
-                        defaultValue={[2,6]}
-                        aria-labelledby="discrete-slider-custom"
-                        marks={marksCleanliness}
-                        min={0}
-                        max={9}/>
-                </div>
-                <div className={css.bottomBlockWrapper}>
-                    <div className={css.groupFormFlags}>
-                        <img src={flagAM} alt="AM"/>
-                        <img src={flagRU} alt="RU"/>
-                        <img src={flagUSA} alt="USA"/>
-                    </div>
-                    <div className={css.FormInputWrapper}>
-                        <div className={css.internalName}>
-                            <p className={css.formInputTitle}>
-                                внутренняя имя
-                            </p>
-                            <input type="text" value={internalNameValue} onChange={internalNameValueHandler}/>
-                        </div>
-                        <div className={css.forSaleName}>
-                            <p className={css.formInputTitle}>
-                                для продаже
-                            </p>
-                            <input type="text" value={forSaleNameValue} onChange={forSaleNameValueHandler}/>
-                            <button type='submit' className={css.groupformButton}>подтвердить</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
+      <div className={css.groupForm}>
+        <div className={css.groupFormHeader}>
+          <p className={css.groupFormTitle}>
+            группы
+          </p>
+          <span className={css.gropeFormClose} onClick={formCloser}>
+            <img src={CloseIcon} alt="close"/>
+          </span>
         </div>
+        <form className={css.groupFormContent} onSubmit={formHandler}>
+          <div className={css.groupformShade}>
+            <p className={css.sliderTitle}>
+              оттенок
+            </p>
+            <Slider
+                onChange={shadeHandleChanger}
+                defaultValue={[1,4]}
+                aria-labelledby="discrete-slider-custom"
+                marks={marksShade}
+                min={0}
+                max={6}
+            />
+          </div>
+          <div className={css.groupformCleanliness}>
+            <p className={css.sliderTitle}>
+              Чистота
+            </p>
+            <Slider
+                onChange={cleanlinessHandleChanger}
+                defaultValue={[2,6]}
+                aria-labelledby="discrete-slider-custom"
+                marks={marksCleanliness}
+                min={0}
+                max={9}/>
+          </div>
+          <div className={css.bottomBlockWrapper}>
+            <div className={css.groupFormFlags}>
+              <img src={flagAM} alt="AM"/>
+              <img src={flagRU} alt="RU"/>
+              <img src={flagUSA} alt="USA"/>
+            </div>
+            <div className={css.FormInputWrapper}>
+              <div className={css.internalName}>
+                <p className={css.formInputTitle}>
+                  внутренняя имя
+                </p>
+                <input type="text"  onChange={internalNameValue}/>
+              </div>
+              <div className={css.forSaleName}>
+                <p className={css.formInputTitle}>
+                  для продаже
+                </p>
+                <input type="text" onChange={forSaleNameValue}/>
+                <button type='submit' className={css.groupformButton}>подтвердить</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     )
 };
 
--- src/components/groups-block/groups-block.js
+++ src/components/groups-block/groups-block.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import css from './groups-block.module.css'
 import flagAM from '../../icons/png/flag-AM.png'
@@ -8,8 +8,91 @@ import GroupForm from "./GroupeForm/group-form";
 
 import brilliantWhite from '../../images/white_brilliant_image.png'
 import brilliantCircle from '../../images/brilliant_form_circle.png'
+import brilliantSizeIcon from '../../icons/png/brilliant-size-icon.png'
 
 const GroupBlock = () => {
+  let [isFormOpen, setIsFormOpen] = useState(false)
+  let [qualityData, setQualityData] = useState([])
+  let [priceData, setPriceData] = useState([])
+  let [shadeValue, setNewShadeValue] = useState([1,4]);
+  let [cleanlinessValue, setNewCleanlinessValue] = useState([2,6]);
+  let [internalName, setInternalName] = useState('');
+  let [forSaleName, setForSaleName] = useState('');
+
+
+  const formSubmit = (e) => {
+    e.preventDefault()
+    let shadeHandle = [];
+    let cleanlinessHandle = [];
+    switch (shadeValue[0]) {
+      case 0 : shadeHandle[0] = 'D'; break;
+      case 1 : shadeHandle[0] = 'E'; break;
+      case 2 : shadeHandle[0] = 'F'; break;
+      case 3 : shadeHandle[0] = 'G'; break;
+      case 4 : shadeHandle[0] = 'H'; break;
+      case 5 : shadeHandle[0] = 'I'; break;
+      case 6 : shadeHandle[0] = 'J'; break;
+    }
+    switch (shadeValue[1]) {
+      case 0 : shadeHandle[1] = 'D'; break;
+      case 1 : shadeHandle[1] = 'E'; break;
+      case 2 : shadeHandle[1] = 'F'; break;
+      case 3 : shadeHandle[1] = 'G'; break;
+      case 4 : shadeHandle[1] = 'H'; break;
+      case 5 : shadeHandle[1] = 'I'; break;
+      case 6 : shadeHandle[1] = 'J'; break;
+    }
+    switch (cleanlinessValue[0]) {
+      case 0 : cleanlinessHandle[0] = 'IF'; break;
+      case 1 : cleanlinessHandle[0] = 'VVS!'; break;
+      case 2 : cleanlinessHandle[0] = 'VVS2'; break;
+      case 3 : cleanlinessHandle[0] = 'VS1'; break;
+      case 4 : cleanlinessHandle[0] = 'VS2'; break;
+      case 5 : cleanlinessHandle[0] = 'FL'; break;
+      case 6 : cleanlinessHandle[0] = 'PL'; break;
+      case 7 : cleanlinessHandle[0] = 'P2'; break;
+      case 8 : cleanlinessHandle[0] = 'SI1'; break;
+      case 9 : cleanlinessHandle[0] = 'SI2'; break;
+    }
+    switch (cleanlinessValue[1]) {
+      case 0 : cleanlinessHandle[1] = 'IF'; break;
+      case 1 : cleanlinessHandle[1] = 'VVS!'; break;
+      case 2 : cleanlinessHandle[1] = 'VVS2'; break;
+      case 3 : cleanlinessHandle[1] = 'VS1'; break;
+      case 4 : cleanlinessHandle[1] = 'VS2'; break;
+      case 5 : cleanlinessHandle[1] = 'FL'; break;
+      case 6 : cleanlinessHandle[1] = 'PL'; break;
+      case 7 : cleanlinessHandle[1] = 'P2'; break;
+      case 8 : cleanlinessHandle[1] = 'SI1'; break;
+      case 9 : cleanlinessHandle[1] = 'SI2'; break;
+    }
+
+    setQualityData([...qualityData, {
+      shade: shadeHandle.join(''),
+      cleanliness: cleanlinessHandle.join(''),
+      price : ''
+    }])
+
+    formClose()
+  }
+  const shadeHandleChange = (event, newShadeValue) => {
+    setNewShadeValue(newShadeValue);
+  };
+  const cleanlinessHandleChange = (event, newCleanlinessValue) => {
+    setNewCleanlinessValue(newCleanlinessValue);
+  };
+  const internalNameValueHandler = (e) => {
+    setInternalName(e.target.value)
+  };
+  const forSaleNameValueHandler = (e) => {
+    setForSaleName(e.target.value)
+  };
+  const formOpen = () => {
+    setIsFormOpen(true)
+  }
+  const formClose = () => {
+    setIsFormOpen(false)
+  }
   return (
     <div className={css.groupBlock}>
       <p className={css.groupBlockTitle}>группы</p>
@@ -22,45 +105,79 @@ const GroupBlock = () => {
         <table className={css.infoTable}>
           <thead>
             <tr>
-                <th>
-                    <select name="" id="">
-                        <option value="USD">USD</option>
-                        <option value="RU">RU</option>
-                        <option value="AM">AM</option>
-                    </select>
-                </th>
-                <th><p className={css.tableTitle}>качества</p></th>
+              <th>
+                <select name="" id="">
+                  <option value="USD">USD</option>
+                  <option value="RU">RU</option>
+                  <option value="AM">AM</option>
+                </select>
+              </th>
+              <th colSpan='200'><p className={css.tableTitle}>качества</p></th>
             </tr>
           </thead>
           <tbody>
-           <tr>
-               <td className={css.imageSection}>
-                   <div>
-                       <img src={brilliantWhite} alt="brilliant color"/>
-                       <p>белый</p>
-                   </div>
-                   <div>
-                       <img src={brilliantCircle} alt="brilliant form"/>
-                       <p>круглый</p>
-                   </div>
-               </td>
-               <td><span className={css.plusIcon}>+</span></td>
-           </tr>
+            <tr>
+              <td className={css.imageSection}>
+                <div>
+                  <img src={brilliantWhite} alt="brilliant color"/>
+                  <p>белый</p>
+                </div>
+                <div>
+                  <img src={brilliantCircle} alt="brilliant form"/>
+                  <p>круглый</p>
+                </div>
+              </td>
+              {qualityData.map((item, index) => {
+                return (
+                  <td key={index}>
+                    <p>{`${item.shade}-${item.cleanliness}`}</p>
+                    <p></p>
+                  </td>
+                  )
+              })}
+              <td>
+                <span className={css.plusIcon} onClick={formOpen}>
+                  +
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <table className={css.priceTable}>
+          <thead>
+            <tr>
+              <th><img src={brilliantSizeIcon} alt="size icon"/></th>
+              <th colSpan="200"><p className={css.tableTitle}>цена</p></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>
+                <span></span>
+              </td>
+              {qualityData.map((item,index) => {
+                return (
+                    <td key={index}><span>{item.price}</span></td>
+                )
+              })}
+              <td>
+                <span></span>
+              </td>
+            </tr>
+
           </tbody>
 
-        </table>
-        <table>
-            <thead>
-            <tr>
-                <th>2</th>
-                <th>2</th>
-            </tr>
-            </thead>
 
-        </table>
+       </table>
       </div>
 
-      {/*<GroupForm />*/}
+      {isFormOpen ? <GroupForm formCloser={formClose}
+                                cleanlinessHandleChanger={cleanlinessHandleChange}
+                                shadeHandleChanger={shadeHandleChange}
+                                internalNameValue={internalNameValueHandler}
+                                forSaleNameValue={forSaleNameValueHandler}
+                                formHandler = {formSubmit}/> : null}
     </div>
   )
 };
